8200d7a42019c8eeb79659bd76eb3857f6cb63eb
Josh Sandlin <joshua.sandlin@gmail.com>
2015-10-24 18:37:35 -0400

    finished spec and converted internal methods to be private

--- lib/diceware.rb
+++ lib/diceware.rb
@@ -1,13 +1,42 @@
 class Diceware
+
+  def initialize
+    @wordlist = WordList.new('/usr/share/dict/words')
+  end
+
+  def generate_passphrase
+    get_word_list.join(" ").gsub("\n", "")
+  end
+
+  private
+
   def roll
     Random.rand(6)
   end
 
   def generate_word_index
-    Array.new(5) { self.roll }.join("").to_i
+    Array.new(5) { roll }.join("").to_i
   end
 
   def generate_word_index_list
-    Array.new(4) { self.generate_word_index }
+    Array.new(4) { generate_word_index }
+  end
+
+  def get_word
+    @wordlist.get_word_by_index(generate_word_index)
+  end
+
+  def get_word_list
+    Array.new(4) { @wordlist.get_word_by_index(generate_word_index) }
+  end
+end
+
+class WordList
+  def initialize(wordlist)
+    @wordlist = File.read(wordlist).lines
+  end
+
+  def get_word_by_index(index)
+    @wordlist[index]
   end
 end
--- spec/diceware_spec.rb
+++ spec/diceware_spec.rb
@@ -2,6 +2,10 @@ require_relative '../lib/diceware.rb'
 
 describe Diceware do
 
+  before(:all) do
+    @diceware = Diceware.new
+  end
+
   context "#new" do
     it "can be instantiated" do
       expect(Diceware).to receive(:new)
@@ -11,26 +15,45 @@ describe Diceware do
 
   context "#roll" do
     it "can roll the dice once" do
-      diceware = Diceware.new
-      roll = diceware.roll
-      expect(roll).to be_an(Integer)
+      roll = @diceware.send(:roll)
+      expect(roll).to be_an Integer
       expect(roll).to be <= 6
     end
   end
 
   context "#generate_word_index" do
     it "will roll five times and return a five-digit number" do
-      diceware = Diceware.new
-      word_index = diceware.generate_word_index
+      word_index = @diceware.send(:generate_word_index)
       expect(word_index.to_s.size).to eql 5
     end
   end
 
   context "#generate_word_index_list" do
     it "will output an array of five word indices" do
-      diceware = Diceware.new
-      word_index_arr = diceware.generate_word_index_list
+      word_index_arr = @diceware.send(:generate_word_index_list)
       expect(word_index_arr.size).to eql 4
+    end
+  end
+
+  context "#get_word" do
+    it "will get a word from the wordlist by index" do
+      word = @diceware.send(:get_word)
+      expect(word).to be_a String
+    end
+  end
+
+  context "#get_word_list" do
+    it "will get four words from the wordlist by indices" do
+      wordlist = @diceware.send(:get_word_list)
+      expect(wordlist.size).to eql 4
+      expect(wordlist[0]).to be_a String
+    end
+  end
+
+  context "#generate_passphrase" do
+    it "will generate a passhrase using the Diceware method" do
+      passphrase = @diceware.generate_passphrase
+      expect(passphrase).to be_a String
     end
   end
 
